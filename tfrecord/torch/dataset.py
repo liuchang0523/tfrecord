94e1199d8ecb87766de3e3d4b343d0cf6314f811
liuchang <liuchang@orbbec.com>
2021-12-27 17:47:44 +0800

    修复num_workers!=0时，重复读取数据的问题

--- tfrecord/torch/dataset.py
+++ tfrecord/torch/dataset.py
@@ -165,10 +165,14 @@ class MultiTFRecordDataset(torch.utils.data.IterableDataset):
     def __iter__(self):
         worker_info = torch.utils.data.get_worker_info()
         if worker_info is not None:
+            shard = worker_info.id, worker_info.num_workers
             np.random.seed(worker_info.seed % np.iinfo(np.uint32).max)
+        else:
+            shard = None
         it = reader.multi_tfrecord_loader(data_pattern=self.data_pattern,
                                           index_pattern=self.index_pattern,
                                           splits=self.splits,
+                                          shard=shard,
                                           description=self.description,
                                           sequence_description=self.sequence_description,
                                           compression_type=self.compression_type,
